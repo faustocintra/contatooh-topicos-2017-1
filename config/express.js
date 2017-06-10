faac4f94c75ba674efda24a27a17764c8aa4bd7d
saul ambrosio <saulezequiel29@gmail.com>
2017-06-09 22:11:57 -0300

    add o helmetincluir o modulo helmet e os add o helme middlewares necessários

--- config/express.js
+++ config/express.js
@@ -7,6 +7,9 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var passport = require('passport');
 
+// Incluir modulo 'helmet'
+var helmet = require('helmet');
+
 module.exports = function () {
    var app = express();
    app.set('port', 3000);
@@ -27,6 +30,23 @@ module.exports = function () {
    }));
    app.use(passport.initialize());
    app.use(passport.session());
+
+    //Ativar middleware 'frameguard'
+    // É através do middleware helmet.xframe que evitamos que nossas páginas sejam referenciadas por <frame> ou <iframe>.
+   app.use(helmet.frameguard());
+
+   //Esse código adiciona o header htpp X-XSS-Protection 
+   // O header solicita ao navegador a ativação de uma proteção especial contra XSS.
+   app.use(helmet.xssFilter());
+
+    //Esse código adiciona o noSniff 
+    //que impede o carregamento através das tags link e script arquivos que não sejam dos MIME types text/css e
+    //text/javascript
+   app.use(helmet.noSniff());
+
+   //Ativar middleware que desabilita 'x-powered-by', para deixar de exibir 
+   //qual tecnologias está sendo utilizada pelo servidor.
+   app.disable('x-powered-by');
 
    //home(app);
    load('models', {cwd: 'app'})
