ce3801cbb93105848fc09a865fe6ed1de1a34ed2
Diego Cintra <diegoffcintra@gmail.com>
2017-06-09 22:04:17 -0300

    Inclusão do pacote helmet, e aplicação de middlewares básicas

--- config/express.js
+++ config/express.js
@@ -7,6 +7,9 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var passport = require('passport');
 
+// fazendo a requisição do helmet
+var helmet = require('helmet');
+
 module.exports = function () {
    var app = express();
    app.set('port', 3000);
@@ -27,6 +30,25 @@ module.exports = function () {
    }));
    app.use(passport.initialize());
    app.use(passport.session());
+
+   /*este comando desabilita as informações do servidor o qual a aplicação
+     está hospedada, para evitar possíveis vulnerabilidades.*/
+   app.disable('x-powered-by');
+
+   /*comando responsável por impedir que a página seja incorporada em iframes */
+   app.use(helmet({
+      frameguard: {
+        action: 'deny'
+      }
+   }));
+
+   /*Este filtro previne ataques do tipo xss, e impede que sejam inseridos scripts maliciosos*/
+   app.use(helmet.xssFilter());
+
+   /*Este comando não permite que arquivos que não sejam do tipo text/css e text/javascript sejam
+   incluídos na página */
+   app.use(helmet.nosniff());
+
 
    //home(app);
    load('models', {cwd: 'app'})
