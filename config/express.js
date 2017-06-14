3b44f8d040de841092c2d77942a8e564fc69d288
Danilo Matias <danilo.maldito@gmail.com>
2017-06-14 20:42:38 -0300

    Added security changes of chapter 9.

--- config/express.js
+++ config/express.js
@@ -7,6 +7,8 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var passport = require('passport');
 
+var helmet = require('helmet');
+
 module.exports = function () {
    var app = express();
    app.set('port', 3000);
@@ -28,9 +30,16 @@ module.exports = function () {
    app.use(passport.initialize());
    app.use(passport.session());
 
+   app.use(helmet());
+   app.use(helmet.xframe());
+   app.use(helmet.xssFilter());
+   app.use(helmet.nosniff());
+   app.disable('x-powered-by');
+
    //home(app);
    load('models', {cwd: 'app'})
       .then('controllers')
+      .then('routes/auth.js')
       .then('routes')
       .into(app);
    return app;
