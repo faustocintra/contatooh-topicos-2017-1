77a705036c639e005dad88500bfbc2393fcbaa2e
Danilo Matias <danilo.maldito@gmail.com>
2017-06-14 20:43:51 -0300

    Added sanitization process on Contato.js.

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -1,4 +1,5 @@
 module.exports = function(app) {
+   var sanitize = require('mongo-sanitize');
    var controller = {};
    
    var Contato = app.models.contato;
@@ -33,7 +34,8 @@ module.exports = function(app) {
    };
 
    controller.removeContato = function(req, res) {
-		Contato.remove({_id: req.params.id}).exec().then(
+    var _id = sanitize(req.params.id);
+		Contato.remove({"_id": _id}).exec().then(
 			function() {
 				// HTTP 204: OK, sem conteúdo a seguir
 				res.status(204).end();
@@ -45,8 +47,16 @@ module.exports = function(app) {
    }
 
    controller.salvaContato = function(req, res) {
-		if(req.body._id) { // Atualização
-			Contato.findByIdAndUpdate(req.body._id, req.body).exec()
+    var _id = req.body._id;
+
+    var dados = {
+      "nome" : req.body.nome,
+      "email" : req.body.email,
+      "emergencia" : req.body.emergencia || null
+    };
+
+		if(_id) { // Atualização
+			Contato.findByIdAndUpdate(_id, dados).exec()
 				.then({
 					function(contato) {
 						res.json(contato);
@@ -58,7 +68,7 @@ module.exports = function(app) {
 				});
 		}
 		else { // Inserção
-			Contato.create(req.body).then(
+			Contato.create(dados).then(
 				function(contato) {
 					// HTTP 201: criado					
 					res.status(201).json(contato);
